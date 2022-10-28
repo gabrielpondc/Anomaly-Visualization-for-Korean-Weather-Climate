912adc27ee1f73abf944adaa3fb0c673fd304d0f
gabrielpondc <gabrielpondc@yahoo.com>
2022-10-29 00:17:29 +0900

    add

--- api.php
+++ api.php
@@ -1,16 +1,16 @@
 <?php 
 ini_set("max_execution_time",10000);
-$set_charset = 'export LANG=ko_KR.UTF-8;';
+
 set_time_limit(10000);
 $path=$_GET['path'];
 $key=$_GET['key'];
-$cmd=$set_charset.'/usr/bin/python3 /media/ubuntu/Newdisk/xyo/admin/localhost_80/wwwroot/graphemedding.py '.$path.' '.$key.'  2> error.txt';
+$cmd='F:\Users\caucse\AppData\Local\Programs\Python\Python38\python.exe F:\avwc\graphemedding.py '.$path.' '.$key.'  2> error.txt';
 $l = exec($cmd,$res,$ret);
 class result{
-    public $anomalytimeinterval;
+    public $abnormaltimeinterval;
 }
 $result=new result();
-$result->anomalytimeinterval=$l;
+$result->abnormaltimeinterval=$l;
 $data = json_encode($result,JSON_UNESCAPED_SLASHES);
 echo $data;
 ?>
